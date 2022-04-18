5d253a75f23b9153530ad1f5098d0870b37153bd
邱博 <qiubo@sobey.com>
2022-04-18 16:49:00 +0800

    rabbitMQ项目

---
+++ src/main/java/com/qb/consumer/Consumer.java
@@ -0,0 +1,10 @@
+package com.qb.consumer;
+
+/**
+ * @author qiuBo
+ * @Description: todo
+ * @date 2022/4/18 16:18
+ */
+public class Consumer {
+
+}
---
+++ src/main/java/com/qb/porducer/Porducer.java
@@ -0,0 +1,10 @@
+package com.qb.porducer;
+
+/**
+ * @author qiuBo
+ * @Description: todo
+ * @date 2022/4/18 16:18
+ */
+public class Porducer {
+
+}
